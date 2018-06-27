6ca90368e6cce65b6915400acbd7f43bc882d73c
luckymichaln <michalnowak.contact@gmail.com>
2018-06-27 08:39:04 +0200

    fix home-page bricks :hover

--- src/components/common/to-hp-gallery-block.vue
+++ src/components/common/to-hp-gallery-block.vue
@@ -206,12 +206,64 @@
         align-self: flex-end;
       }
 
-      a,
       .brick-nolink {
         position: relative;
         display: block;
         height: 100%;
         width: 100%;
+        cursor: default;
+      }
+
+      a:not(.brick-nolink) {
+        position: relative;
+        display: block;
+        height: 100%;
+        width: 100%;
+
+        &::before,
+        &::after {
+          content: '';
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+        }
+
+        &::before {
+          height: calc(100% - 4px);
+          background-color: rgba(#000, .5);
+          opacity: 0;
+          transition: .3s;
+        }
+
+        &::after {
+          z-index: 100;
+          background-color: darken(#fff, 2);
+          transition: 1s;
+        }
+
+        &[lazy=loading] {
+          &::after  {
+            opacity: 1;
+          }
+        }
+
+        &[lazy=loaded] {
+          &::after {
+            opacity: 0;
+          }
+        }
+
+        &:hover {
+          .brick-info {
+            opacity: 1;
+          }
+
+          &::before {
+            opacity: 1;
+          }
+        }
       }
 
 
